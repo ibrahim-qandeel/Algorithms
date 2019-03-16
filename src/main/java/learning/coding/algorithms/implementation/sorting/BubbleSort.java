f55d08955a9fbb6d9b2bdca1501b0e997c42c444
Ibrahim Qandeel <iqandeel@sejeltech.com>
2019-03-16 14:55:44 +0300

    Bubble Sort Algorithm Enhancement

--- src/main/java/learning/coding/algorithms/implementation/sorting/BubbleSort.java
+++ src/main/java/learning/coding/algorithms/implementation/sorting/BubbleSort.java
@@ -3,15 +3,19 @@ package learning.coding.algorithms.implementation.sorting;
 public class BubbleSort {
 
   public static void sort(int array[]) {
-
     int length = array.length;
+    boolean isSwapped = false;
     for (int i = 0; i < array.length; i++) {
       for (int j = 0; j < length - 1; j++) {
         if (array[j] > array[j + 1]) {
           int temp = array[j];
           array[j] = array[j + 1];
           array[j + 1] = temp;
+          isSwapped = true;
         }
+      }
+      if (!isSwapped) {
+        break;
       }
       length--;
     }
