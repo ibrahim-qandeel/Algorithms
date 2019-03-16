ee4fd77702333ae80838a6a24d7663be660c7a9d
Ibrahim Qandeel <iqandeel@sejeltech.com>
2019-03-16 23:38:23 +0300

    Insertion Sort Algorithm

--- src/main/java/learning/coding/algorithms/Main.java
+++ src/main/java/learning/coding/algorithms/Main.java
@@ -1,12 +1,17 @@
 package learning.coding.algorithms;
 
+import learning.coding.algorithms.implementation.sorting.InsertionSort;
 import learning.coding.algorithms.problems.GeeksForGeeks.SortTheArray;
 
 public class Main {
 
   public static void main(String[] args) {
     //GeeksForGeeks
-    SortTheArray.sortTheArray();
+//    SortTheArray.sortTheArray();
+
+    int data[] = {9, 4, 8, 5};
+    InsertionSort.sort(data);
+    printArray(data);
   }
 
   public static void printArray(int[] array) {
